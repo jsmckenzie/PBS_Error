07883d9bdf3507d79f4f06e51e08c81c4e80beea
J McKenzie <jsmckenzie@users.noreply.github.com>
2023-05-03 21:22:13 +0100

    Update ErrLog.sh
    Added all flags, fully tested on examples.

--- ErrLog.sh
+++ ErrLog.sh
@@ -1,37 +1,68 @@
 #!/bin/bash
 
+# Set the default path to be the current directory
+filePath=$(pwd)
+
+# Default file save path
+outPath="LogFile.csv"
+
 # Flags, such as job number
-while getopts J: flag
+while getopts P:J:O: flag;
 do
     case "${flag}" in
-        J) job=${OPTARG};;
+        J) # specified job number of part of file name
+            job="${OPTARG}"
+            ;;
+        P) # where to look
+            filePath="$OPTARG"
+            ;;
+
+        O) # save the file here
+            outPath="$OPTARG"
+            ;;
+        ?) # error if unknown
+            echo "Error"
+            exit 1
+            ;;
     esac
 done
 
-#echo $job
-
 # List of all files (use job flag to trim as required)
-#allF=$(find /Users/jmckenzi/Dropbox/ICL/BashTest -name '*.ER')
-allF=$(find /Users/jmckenzi/Dropbox/ICL/BashTest -name '*.ER' | grep "$job")
+allF=$(find "$filePath" -name '*.ER' | grep "$job")
 
 # Create an empty megafile
-touch LogFile.csv
-echo "File,Error" > LogFile.csv
+touch "$outPath"
+echo "Path,File,Job,Array,Error" > "$outPath"
 
 # Loop through each one
-for FILE in $allF
+for FILE in $allF 
+do  
+    # File contents
+    fc=$(cat $FILE)
+
+    # Split path and name
+    thisPath=$(dirname "$FILE")
+    thisName=$(basename "$FILE")
+        
+    # If this is an array job, it will have square brackets
+    if [[ "$thisName" == *"["* ]]; then
+    
+        # Now split job number too
+        jobID="${thisName%\[*}"
+        arrID="${thisName#*\[}"
+        arrIE="${arrID%\]*}"
+        
+    else
+        
+        # If not an array, then just get the job
+        jobID="${thisName%.pbs*}"
+        arrIE=""
+        
+    fi
   
-  #do cat $FILE;
-  #do
-
-
-  # File contents
-  do fc=$(cat $FILE)
-
-  # Write file name to file
-  echo "\"$FILE\",\"$fc\"" >> LogFile.csv
+    # Write all information to the file
+    echo "\"$thisPath\",\"$thisName\",\"$jobID\",\"$arrIE\",\"$fc\"" >> "$outPath"
   
 done
 
-
-#cat LogFile.csv
+echo "Error log written to "$outPath
