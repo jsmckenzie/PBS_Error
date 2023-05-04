1ddd87adcf9a6ec3d445ffe4649630b333d1a0d1
J McKenzie <jsmckenzie@users.noreply.github.com>
2023-05-04 09:54:32 +0100

    Validate test results
    Script that checks output files are the same to the test case files.

---
+++ testResults.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Compare the output files with the test files
+
+# List all of the file path/names here
+test1="tests/Test_Full.csv"
+test2="tests/Test_744.csv"
+test3="tests/Test_Array4.csv"
+
+cmp1="testResults/Test_Full.csv"
+cmp2="testResults/Test_744.csv"
+cmp3="testResults/Test_Array4.csv"
+
+# Result counter
+res=0
+
+# File 1 (full file)
+if cmp --silent -- "$test1" "$cmp1"; then
+  echo "Test 1 PASS"
+  res=$((res+1))
+else
+  echo "Test 1 FAIL"
+fi
+
+# File 2 (744*)
+if cmp --silent -- "$test2" "$cmp2"; then
+  echo "Test 2 PASS"
+  res=$((res+1))
+else
+  echo "Test 2 FAIL"
+fi
+
+# File 1 ([4)
+if cmp --silent -- "$test3" "$cmp3"; then
+  echo "Test 3 PASS"
+  res=$((res+1))
+else
+  echo "Test 3 FAIL"
+fi
+
+# Check if all tests completed successfully
+if [ $res -eq 3 ]; then
+  echo "Pass"
+  exit 0
+else
+  echo "Fail"
+  exit 1
+fi
