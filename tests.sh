cbba1795952ba89975be1b53f37f5fd36a87c161
J McKenzie <jsmckenzie@users.noreply.github.com>
2023-05-04 10:36:54 +0100

    Test files updated
    Updated paths for CI

--- tests.sh
+++ tests.sh
@@ -3,13 +3,13 @@
 # Run a series of tests with different commands, in order to ensure that the
 # function works as expected
 
-mkdir tmp
+mkdir testResults/
 
 # All files
-bash ErrLog.sh -P examples -O tmp/Test_Full.csv
+bash ErrLog.sh -P examples -O testResults/Test_Full.csv
 
 # Job 744* only
-bash ErrLog.sh -P examples -O tmp/Test_744.csv -J 744
+bash ErrLog.sh -P examples -O testResults/Test_744.csv -J 744
 
 # Array jobs beginning with [4~
-bash ErrLog.sh -P examples -O tmp/Test_Array4.csv -J '\[4'
+bash ErrLog.sh -P examples -O testResults/Test_Array4.csv -J '\[4'
